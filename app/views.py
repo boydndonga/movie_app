814695e687a8ac3e67fb2c3c51cf8aa5fa345154
Boyd Ndonga <boydbugle@gmail.com>
2018-08-31 01:45:22 +0300

    configure root view to show movies from api

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from app import app
 from flask import render_template
+from .request import get_movies
+
 
 @app.route("/")
 def index():
@@ -8,5 +10,8 @@ def index():
     define view for root page
 
     """
-    message = "hello Boyd"
-    return render_template('index.html', message=message)
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title=title, popular=popular_movies)
