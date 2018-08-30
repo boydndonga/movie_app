e34b536dc905c07e2ac13080b80105ee1768826e
Boyd Ndonga <boydbugle@gmail.com>
2018-08-31 00:51:12 +0300

    create view function and template for root page

--- app/views.py
+++ app/views.py
@@ -1,2 +1,12 @@
 from app import app
 from flask import render_template
+
+@app.route("/")
+def index():
+    '''
+
+    define view for root page
+
+    '''
+
+    return render_template('index.html')
