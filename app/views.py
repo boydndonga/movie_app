af349750013249f234acbfafc63c968975222a78
Boyd Ndonga <boydbugle@gmail.com>
2018-08-31 03:34:28 +0300

    import model into views

--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from app import app
 from flask import render_template, request, redirect, url_for
 from .request import get_movies, get_movie, search_movie
+from .models import reviews
 from .forms import ReviewForm
 
+
+Review = reviews.Review
 
 @app.route("/")
 def index():
