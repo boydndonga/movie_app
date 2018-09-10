611cf0986999ee2935058ddaca94b2952b381f08
Boyd Ndonga <boydbugle@gmail.com>
2018-09-10 17:11:12 +0300

    create a getter for users

--- app/models.py
+++ app/models.py
@@ -1,10 +1,14 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     pass_secure = db.Column(db.String(255))
 
@@ -22,6 +26,10 @@ class User(db.Model):
 
     def verify_password(self,password):
         return check_password_hash(self.pass_secure,password)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
 class Role(db.Model):
     __tablename__ = 'roles'
