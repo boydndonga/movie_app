c61c9512e25da748508bd74f43e29016cb733ef2
Boyd Ndonga <boydbugle@gmail.com>
2018-09-10 13:02:25 +0300

    import User class model and db into manager

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 import unittest
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
-
+from app.models import User
 
 # Creating app instance
 app = create_app('development')
