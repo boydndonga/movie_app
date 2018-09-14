5e4e727ea334073d73eb43797d3a01586059f3c1
Boyd Ndonga <boydbugle@gmail.com>
2018-09-14 23:16:07 +0300

    update review form to remove validators

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,8 +5,8 @@ from wtforms.validators import Required
 
 class ReviewForm(FlaskForm):
 
-    title = StringField('Review title',validators=[Required()])
-    review = TextAreaField('Movie review', validators=[Required()])
+    title = StringField('Review title', validators=[Required()])
+    review = TextAreaField('Movie review')
     submit = SubmitField('Submit')
 
 
