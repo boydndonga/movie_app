6daf1e2465ffd690c54399b9a4367d7e434ee8b1
Boyd Ndonga <boydbugle@gmail.com>
2018-09-14 23:51:50 +0300

    configure dev url

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 export MOVIE_API_KEY='12b82083888e210efd30e86a74ad2daf'
-export MAIL_USERNAME=boydbugle@gmail.com
-export MAIL_PASSWORD=pirahnas
-
+export MAIL_USERNAME=
+export MAIL_PASSWORD=
 python3.6 manage.py server
